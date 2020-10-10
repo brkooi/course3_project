dffddbd3d71e50ea38a1148c872f3aa10d1879d7
Ben Kooi <brkooi@hotmail.com>
2020-10-11 00:11:52 +0200

    Last commit for peer review

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,10 @@
+# Data science Course 3 - Getting and Cleaning data
+# Final assignment
+# Script run_analysis.R
+
+
 #load packages
-packages <- c("data.table")
+packages <- c("data.table","dplyr")
 sapply(packages, require, character.only=TRUE, quietly=TRUE)
 
 # download file and unzip datafiles
@@ -20,15 +25,41 @@ test_data <- fread("UCI HAR Dataset/test/X_test.txt")
 test_activities <- fread("UCI HAR Dataset/test/Y_test.txt")
 test_subjects <- fread("UCI HAR Dataset/test/subject_test.txt")
 
+# extract labels
+activity_labels <- setNames(activity_labels, c("activity_id", "activity"))
+feature_labels <- unlist(features[,2])
 
 # set columnnames
-activity_labels <- setNames(activity_labels, c("activity_id", "activity"))
-features <- setNames(features, c("feature_id", "feature"))
+training_data <- setnames(training_data,c(feature_labels))
 training_activities <- setNames(training_activities, c("activity_id"))
+training_activities <- left_join(training_activities, activity_labels)
 training_subjects <- setNames(training_subjects, c("subject_id"))
+
+test_data <- setnames(test_data,c(feature_labels))
+test_activities <- setNames(test_activities, c("activity_id"))
+test_activities <- left_join(test_activities, activity_labels)
+test_subjects <- setNames(test_subjects, c("subject_id"))
 
 # combine datasets
 training_raw <- cbind(training_subjects, training_activities, training_data)
 test_raw <- cbind(test_subjects, test_activities, test_data)
 
-# tidy datasets
+#merge training- and test-dataset
+complete_raw <- rbind(training_raw,test_raw)
+
+
+#tidy dataset
+complete_tidy <- complete_raw  %>% select(subject_id, activity, contains("mean") | contains("std"))
+
+colnames(complete_tidy) <- gsub("\\(", "", colnames(complete_tidy))
+colnames(complete_tidy) <- gsub("\\)", "", colnames(complete_tidy))
+colnames(complete_tidy) <- gsub("-", "_", colnames(complete_tidy))
+colnames(complete_tidy) <- gsub(",", "_", colnames(complete_tidy))
+
+#compute mean of every column
+complete_mean <- complete_tidy %>%
+  group_by(subject_id,activity) %>%
+  summarise(across(tBodyAcc_mean_X:fBodyBodyGyroJerkMag_std, mean, na.rm= TRUE))
+
+#write final dataset
+write.csv(complete_mean,"complete_mean.csv", quote=FALSE, row.names=FALSE)
